dcc71901a2a7a6bbecf3d7beb5725ecb524eaedc
yudistrange <yudistrange@gmail.com>
2014-03-07 15:48:17 +0530

    sdasd

--- js/popup.js
+++ js/popup.js
@@ -2,114 +2,23 @@ $(document).ready(function() {
 		var IP=prompt("Please enter server IP\nLeave blank to start local server");
 		if (IP!=null)
   		{
-  			alert("connect to server");
+        beginLocalServer();
+        alert("connect to server");
+  			console.log("connect to server");
   		} else {
-  			alert("start server");
+        beginLocalServer();
+        alert("`server");
+  			console.log("server started");
   		}
 	});
 
 
-/*$(document).ready(function(){
-        $("#RoleBox").dialog({
-        	modal: true,	
-        	buttons: {
-				"Delete all items": function() {
-					$( this ).dialog( "close" );
-				},
-				Cancel: function() {
-					$( this ).dialog( "close" );
-				}
-			},
-			containerCss: {
-            	width: 550,
-            	height: 500
-        	}
-        });
-    });
-*/
-/*$(function() {
-	( "#example" ).dialog({
-			resizable: false,
-			height:140,
-			modal: true,
-			buttons: {
-				"Delete all items": function() {
-					$( this ).dialog( "close" );
-				},
-				Cancel: function() {
-					$( this ).dialog( "close" );
-				}
-			}
-		})
-});*/
-
-/* $(document).ready(function() {
-    $("#dialog").dialog({
-      modal: true
-      buttons: [ 
-      	{ 
-      		text: "Ok", click: function() { 
-      			$( this ).dialog( "close" ); 
-      		} 
-      	} 
-      ]
-    });
-  });
-*/
-/*
-function showModalConfirmDialog(msg, handler) {
-    button1.onclick = function(evt){
-        handler(true);
-    };
-    button2.onclick = function(evt){
-        handler(false);
-    };
-}
-showModalConfirmDialog('Are you sure?', function (outcome) { 
-    alert(outcome ? 'yes' : 'no'); 
-});
-
-*/
-
-/*$(document).ready(function() {
-	var r = confirm("Role");
-	if(r==true) {
-		alert("hat sala");
-	} else {
-		alert("bhak sala");
-	}
-});
-*/
-/*$function() {
-    $( "#dialog-confirm" ).dialog({
-      resizable: false,
-      height:140,
-      modal: true,
-      buttons: {
-        "Delete all items": function() {
-          $( this ).dialog( "close" );
-        },
-        Cancel: function() {
-          $( this ).dialog( "close" );
-        }
-      }
-    });
-  };
-*/
-
-/*$(document).ready( function() {
-	$(".selector").dialog({
-      resizable: false,
-      height:140,
-      modal: true,
-      buttons: {
-        "Delete all items": function() {
-          $( this ).dialog( "close" );
-        },
-        Cancel: function() {
-          $( this ).dialog( "close" );
-        }
-      }
-    });
-});
-*/
+function beginLocalServer() {
+  var http = require('http');
+  http.createServer(function (req, res) {
+      res.writeHead(200, {'Content-Type': 'text/plain'});
+      res.end('Hello World\n');
+  }).listen(1338, '0.0.0.0');
+  alert("Yes");
+  console.log('Server running at http://0.0.0.0:1338/');
+};
